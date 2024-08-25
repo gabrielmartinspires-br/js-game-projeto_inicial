2c60bef908a7dc9e5926ae10ea016c331b410193
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2024-08-25 06:36:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 29;
-prompt('Escolha um número entre 1 e 30')
+let chute = prompt('Escolha um número entre 1 e 30')
+
+if (numeroSecreto == chute) {}
