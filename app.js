4ef732d13c1faf1ef47f5e9876666b954c75b6dc
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2024-08-28 23:46:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,15 +7,5 @@
 // }
 
 alert('Boas vindas ao nosso site!');
-let nome = Lua;
-let idade = 25;
-let numeoDeVenda = 50;
-let saldoDisponivel = 1000;
-let mensagemDeErro = 'Erro! Preencha todos os campos';
-alert(mensagemDeErro);
-let nome = prompt('Digite o seu nome');
-let idade = prompt('Digite a sua idade');
-let idade = prompt('Digite a sua idade');
-if (idade >= 18) {
-    alert('Pode tirar a habilitação!'); 
+
 }
