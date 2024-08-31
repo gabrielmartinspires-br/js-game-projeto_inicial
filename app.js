3a5bd0622db456ba214d9313c15bd34329ae0cf7
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2024-08-31 06:42:44 -0300

    Update app.js

--- app.js
+++ app.js
@@ -17,4 +17,5 @@ let nome = prompt('Digite o seu nome');
 let idade = prompt('Digite a sua idade');
 let idade = prompt('Digite a sua idade');
 if (idade >= 18) {
+    alert('Pode tirar a habilitação!'); 
 }
