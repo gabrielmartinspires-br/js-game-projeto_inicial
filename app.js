73fcbe1a5a4b2289142f921b1ca8b14296cccfdb
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2024-08-31 06:44:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10')
+// alert('Boas vindas ao jogo do número secreto');
+// let numeroSecreto = 5;
+// let chute = prompt('Escolha um número entre 1 e 10')
 
-if (chute == numeroSecreto) {
-    console.log('Isso aí! Você descobriu o número secreto (5)');
-}
+// if (chute == numeroSecreto) {
+//     console.log('Isso aí! Você descobriu o número secreto (5)');
+// }
 
 // alert('Boas vindas ao nosso site!');
 // let nome = Lua;
