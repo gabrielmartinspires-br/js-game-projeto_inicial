93291301c7bc8375a66d4d5b36e8b645e751098b
Gabriel Martins Pires <brgabrielmartins@gmail.com>
2024-09-01 20:28:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let chute = prompt('Escolha um número entre 1 e 10')
 
 // Se o chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    alert('Isso aí! Você descobriu o número secreto (5)');
+    alert(`Isso aí! Você descobriu o número secreto numeroSecreto`);
 } else {
     alert ('Você errou :( ')
 }
